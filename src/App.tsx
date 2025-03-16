521170d93dc2026ce0884ca70a928d169cad05d4
YieldRay <yieldray@outlook.com>
2025-03-17 03:10:42 +0800

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <h1 className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-center text-5xl font-extrabold text-transparent ...">
-      Hello world
-    </h1>
+    <div className="bg-white dark:bg-black text-black dark:text-white h-screen max-h-screen overflow-hidden selection:bg-[#79ffe1] selection:text-black">
+      <div className="fixed inset-0 grid place-content-center p-8 text-lg">
+        <p className="text-center">Hello, world!</p>
+      </div>
+      <p className="fixed bottom-0 left-0 w-full p-8 text-xs text-center text-gray-500 dark:text-gray-400">
+        <span>
+          <code>{JSON.stringify(import.meta.env)}</code>
+        </span>
+      </p>
+    </div>
   );
 }
